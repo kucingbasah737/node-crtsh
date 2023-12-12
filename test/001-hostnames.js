90ae537423efac26b541b473077067a95d580583
Kucing Basah 737 <kucingbasah737@gmail.com>
2023-12-13 02:06:00 +0700

    Tested

--- test/001-hostnames.js
+++ test/001-hostnames.js
@@ -1,15 +1,23 @@
-/* global describe, it */
+/* global describe, it, before */
 
 require('should');
 
 const { hostnames: queryHostnames } = require('../lib');
 
 describe('#hostnames', () => {
-  it('should return correct value', async () => {
-    const hostnames = await queryHostnames('%.example.org');
+  let hostnames;
 
-    hostnames.should.containEql('example.org', 'contain example.org');
-    hostnames.should.containEql('www.example.org', 'contain www.example.org');
-    hostnames.should.not.containEql('abc.example.org', 'not contain abc.example.org');
+  before(async () => {
+    hostnames = await queryHostnames('%.example.org');
+  });
+
+  it('return should contain correct hostnames', () => {
+    hostnames.should.containEql('example.org', 'contain "example.org"');
+    hostnames.should.containEql('www.example.org', 'contain "www.example.org"');
+  });
+
+  it('return should not contain wrong result', () => {
+    hostnames.should.not.containEql('tdreg001.t-systems-sfr.com', 'not contain "tdreg001.t-systems-sfr.com"');
+    hostnames.should.not.containEql('name@example.org', 'not contain "name@example.org"');
   });
 });
